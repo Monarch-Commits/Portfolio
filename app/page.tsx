c7b44f92971739d4e132836f237adef7ad341501
Monarch <158782129+Monarch58272212@users.noreply.github.com>
2026-06-06 08:53:13 +0800

    aaaaaaaaaa

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,11 @@ import Experience from '@/components/experience';
 import Hero from '@/components/hero';
 import FeaturedProject from '@/components/projects';
 import Services from '@/components/services';
-import Test from '@/components/test';
 
 export default function Home() {
   return (
     <main className="bg-background mx-auto w-full max-w-7xl border-x border-white/10">
       <Hero />
-      <Test />
       <FeaturedProject />
       <AboutSection />
       <Experience />
