5b132d9b5152b1e859749d5b3fa4af4bc4c24051
Monarch <158782129+Monarch58272212@users.noreply.github.com>
2026-07-01 07:18:23 +0800

    change the hero image

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { motion } from 'framer-motion';
 import Text from './hero/text';
 import CenterImage from './hero/image';
@@ -13,22 +12,25 @@ export default function Hero() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.3, ease: 'easeOut' }}
-      className="relative mx-auto flex h-auto w-full flex-col items-center justify-between gap-16 overflow-visible border-b border-white/10 bg-black px-4 py-24 sm:px-8 md:py-32 lg:gap-20 lg:px-18 lg:py-50"
+      className="bg-background relative mx-auto flex h-auto w-full flex-col items-center justify-between gap-16 overflow-visible border-b border-white/10 px-4 py-24 sm:px-8 md:py-32 lg:gap-20 lg:px-18 lg:py-50 xl:flex-row"
     >
       <Plus className="absolute -bottom-2 -left-2 z-50 h-4 w-4 text-white" />
       <Plus className="absolute -right-2 -bottom-2 z-50 h-4 w-4 text-white" />
 
-      {/* <div className="absolute inset-0 -z-10 bg-[radial-gradient(circle_at_center,rgba(255,255,255,0.08),transparent_60%)]" /> */}
+      <div className="absolute inset-0 -z-10 bg-[radial-gradient(circle_at_center,rgba(255,255,255,0.08),transparent_60%)]" />
 
       <Text />
-
-      {/* <div className="pointer-events-none absolute inset-0 z-40 w-full bg-linear-to-t from-black via-black/5 to-transparent" /> */}
       <CenterImage />
-      {/* <div className="bg-background absolute top-1/2 -right-24 hidden -translate-y-1/2 rotate-90 border border-white/10 px-4 text-xs text-[10px] font-medium tracking-[0.4em] text-white/80 uppercase lg:block">
-        Simplicity works
-      </div> */}
 
-      {/* <Minimal /> */}
+      <div className="pointer-events-none absolute inset-0 z-40 w-full bg-linear-to-t from-black via-black/5 to-transparent" />
+
+      <div className="bg-background absolute top-1/2 -right-24 hidden -translate-y-1/2 rotate-90 border border-white/10 px-4 text-xs text-[10px] font-medium tracking-[0.4em] text-white/80 uppercase lg:block">
+        Simplicity works
+      </div>
+
+      <Minimal />
+
+      <SocialLinks />
     </motion.section>
   );
 }
